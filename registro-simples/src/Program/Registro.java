f16fea6c19f634671e7f522ae285775cb17549c6
Cailo Pinheiro <cailoisrael@hotmail.com>
2023-02-15 18:01:12 -0300

    gets, sets and constructor for Pessoa.java

---
+++ registro-simples/src/Program/Registro.java
@@ -0,0 +1,8 @@
+package Program;
+import java.util.Scanner;
+public class Registro {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner leitor = new Scanner(System.in);
+    }
+}
