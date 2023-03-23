a778e5f08cf53c5c6c22543f6e43198c0212fb2d
Cailo Pinheiro <cailoisrael@hotmail.com>
2023-03-23 18:42:09 -0300

    first

---
+++ registro-simples/ContadorThread/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ThreadContador cont = new ThreadContador(true);
+        ThreadContador reverseCont = new ThreadContador(false);
+
+        cont.start();
+        reverseCont.start();
+    }
+}
---
+++ registro-simples/ContadorThread/src/ThreadContador.java
@@ -0,0 +1,39 @@
+public class ThreadContador extends Thread {
+    private boolean ordem;
+
+    // Construtor que vai ser exigido na instanciação dessa classe.
+    public ThreadContador(boolean ordem) {
+        this.ordem = ordem;
+    }
+
+    // Sobreescrevendo o método run da classe Thread
+    @Override
+    public void run() {
+
+        if (ordem) {
+
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Thread " + getId() + ": " + i);
+            }
+            try {
+                Thread.sleep(1000);
+            } 
+            catch (Throwable exc) {
+                exc.printStackTrace();
+            }
+
+        } else {
+
+            for (int i = 10; i < 1; i--) {
+                System.out.println("Thread " + getId() + ": " + i);
+            }
+            try {
+                Thread.sleep(2000);
+
+            } 
+            catch (Throwable exc) {
+                exc.printStackTrace();
+            }
+        }
+    }
+}
